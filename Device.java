42d9691acd3cee81759d5c421c735fc48c1036b8
radwanHb <radwanhbobati@gmail.com>
2024-05-18 15:17:10 +0300

    RMI

--- Device.java
+++ Device.java
@@ -1,9 +1,7 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.awt.AWTException;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.net.Socket;
 
 public interface Device extends Remote {
     String captureImage() throws RemoteException, AWTException, IOException;
---
+++ readme.md
@@ -0,0 +1,2 @@
+1] run [Manager.java] for Manager (monitor) role
+2] run [DeviceServer.java] for device (client) role
